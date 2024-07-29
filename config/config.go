abe9cefe7f51c748264f5553d1195553ce25dc57
EmiliodDev <ortizemilio.dev@gmail.com>
2024-07-29 11:08:20 -0600

    first commit

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+    Server struct{
+        Port        int
+    }
+    DB struct {
+        Driver      string
+        Host        string
+        Port        string
+        User        string
+        Password    string
+        Name        string
+    }
+}
+
+var AppConfig Config
+
+func LoadConfig() {
+    viper.SetConfigName("config")
+    viper.SetConfigType("yml")
+    viper.AddConfigPath(".")
+
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatalf("error reading config file, %v", err)
+    }
+
+    if err := viper.Unmarshal(&AppConfig); err != nil {
+        log.Fatalf("unable to decode into a struct, %v", err)
+    }
+}
