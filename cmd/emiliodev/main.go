896ef1b7ced9dc76ee600a9daa5b18bd31c10798
EmiliodDev <ortizemilio.dev@gmail.com>
2024-07-29 13:58:36 -0600

    auto migration table projects

--- cmd/emiliodev/main.go
+++ cmd/emiliodev/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/EmiliodDev/EmilioDev/config"
 	"github.com/EmiliodDev/EmilioDev/internal/db"
+	"github.com/EmiliodDev/EmilioDev/internal/models"
 	"github.com/EmiliodDev/EmilioDev/internal/router"
 )
 
@@ -13,6 +14,10 @@ func main() {
 
 	if err := db.ConnectToDB(); err != nil {
 		log.Fatalf("connection db failed, %v", err)
+	}
+
+	if err := models.Migrate(db.DB); err != nil {
+		log.Fatalf("error running migrations, %v", err)
 	}
 
 	r := router.SetupRouter()
